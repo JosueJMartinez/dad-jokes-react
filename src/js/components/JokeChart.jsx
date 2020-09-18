ba86caa5a74d0b64f602a934f785f505246abd5e
JosueJMartinez <jmarti61@gmail.com>
2020-09-18 03:26:24 +0000

    update

--- src/js/components/JokeChart.jsx
+++ src/js/components/JokeChart.jsx
@@ -9,21 +9,37 @@ export default class JokeChart extends Component {
 	async componentDidMount() {
 		try {
 			let arr = [];
-			let res;
+			let res={};
+			let foundJoke = false;
 			for (let i = 0; i < 10; i++) {
-				res = await Axios.get('https://icanhazdadjoke.com/', {
-					headers: { accept: 'application/json' }
-				});
+				try{
+					do{
+						res = await Axios.get('https://icanhazdadjoke.com/', {
+							headers: { accept: 'application/json' }
+						});
+						if(!res===undefined){
+							throw 'failed retrieving data'
+						}
+						foundJoke = arr.includes(c =>(c.id===res.data.id))
+					}while(foundJoke)
+				}catch(err){
+					console.log('error happen getting intial jokes')	
+				}
+				
 				arr.push(res.data);
 			}
 
-			console.log(arr);
+			this.setState({jokes:arr})
 		} catch (err) {
 			console.log('Error while initially retrieving jokes');
 		}
 	}
 
 	render() {
-		return <div />;
+		console.log(this.state)
+		return <div>
+			{this.state.jokes.map(joke=>(<p key={joke.id} id={joke.id} > {joke.joke} </p>))}
+		</div>
+			
 	}
 }
