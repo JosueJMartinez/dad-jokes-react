31905c19c3427f9e309fb285c8a54c852bf5490f
JosueJMartinez <jmarti61@gmail.com>
2020-09-19 18:03:29 -0500

    refactored

--- src/js/components/JokeList.jsx
+++ src/js/components/JokeList.jsx
@@ -3,6 +3,10 @@ import Axios from 'axios';
 import '../../css/JokeList.css';
 
 export default class JokeList extends Component {
+	static defaultProps = {
+		numJokesToGet: 10
+	};
+
 	state = {
 		jokes: [],
 		isLoading: true
@@ -15,36 +19,39 @@ export default class JokeList extends Component {
 	getJokes = async () => {
 		let res = {};
 		let foundJoke = false;
+		const jokes = [];
 
 		let jokeFunc = id => {
-			return this.state.jokes.some(c => c.id === id);
+			return (
+				this.state.jokes.some(c => c.id === id) ||
+				jokes.some(c => c.id === id)
+			);
 		};
-		try{
-			for (let i = 0; i < 10; i++) {
-				do {
-
-						res = await Axios.get('https://icanhazdadjoke.com/', {
-							headers: { accept: 'application/json' }
-						});
-						if (res.data === undefined)
-							throw new Error('failed retrieving data');
-						// jokeFunc
-						foundJoke = jokeFunc(res.data.id);
-
-
-
-				} while (foundJoke);
-
-				this.setState(prevState => {
-					return {
-						jokes: [ ...prevState.jokes, { ...res.data, rating: 0 } ]
-					};
+		this.setState({ isLoading: true });
+		try {
+			while (jokes.length < this.props.numJokesToGet) {
+				res = await Axios.get('https://icanhazdadjoke.com/', {
+					headers: { accept: 'application/json' }
 				});
-			}	
-		}catch(err){
+
+				if (res.data === undefined)
+					throw new Error('failed retrieving data');
+
+				foundJoke = jokeFunc(res.data.id);
+				if (!foundJoke) {
+					jokes.push({ ...res.data, rating: 0 });
+				}
+			}
+		} catch (err) {
 			console.log('erroro here');
 		}
-		
+		this.setState(prevState => {
+			return {
+				jokes: [ ...prevState.jokes, ...jokes ],
+				isLoading: false
+			};
+		});
+		// this.setState({ isLoading: false });
 	};
 
 	handleClick = e => {
@@ -53,24 +60,28 @@ export default class JokeList extends Component {
 
 	render() {
 		return (
-			<div className='JokeList'>
+			<div className="JokeList">
 				<div className="JokeList-box">
 					<div className="JokeList-button">
 						<button onClick={this.handleClick}>New Jokes</button>
 					</div>
 					<div className="JokeList-list">
-						<ul>
-							{this.state.jokes.map(joke => (
-								<li key={joke.id} id={joke.id}>
-									{' '}
-									{joke.joke} {joke.rating}
-								</li>
-							))}
-						</ul>
+						<h1>Dad Jokes</h1>
+						{this.state.isLoading ? (
+							'loading'
+						) : (
+							<ul>
+								{this.state.jokes.map(joke => (
+									<li key={joke.id} id={joke.id}>
+										{' '}
+										{joke.joke} {joke.rating}
+									</li>
+								))}
+							</ul>
+						)}
 					</div>
 				</div>
 			</div>
-			
 		);
 	}
 }
